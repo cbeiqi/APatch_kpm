863bb3a4b4846614e0e16dfbbc622021c110eef3
cbeiqi <62896828+cbeiqi@users.noreply.github.com>
2024-06-23 00:26:07 +0800

    Update battchg.h

--- qti_battery_charger/battchg.h
+++ qti_battery_charger/battchg.h
@@ -147,3 +147,12 @@ struct module
 	/* Unique handle for this module */
 	char name[64 - sizeof(unsigned long)];
 };
+//umh
+#define UMH_NO_WAIT	0x00	/* don't wait at all */
+#define UMH_WAIT_EXEC	0x01	/* wait for the exec, but not the process */
+#define UMH_WAIT_PROC	0x02	/* wait for the process to complete */
+#define UMH_KILLABLE	0x04	/* wait for EXEC/PROC killable */
+#define UMH_FREEZABLE	0x08	/* wait for EXEC/PROC freezable */
+
+extern int
+call_usermodehelper(const char *path, char **argv, char **envp, int wait);
